e5dc1e13d74039b9b6074bec4489a634deff52f3
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-10-13 03:03:18 +0300

    Fiixed MainActivity layout

--- app/src/main/java/com/vbytsyuk/android/sandbox/MainActivity.kt
+++ app/src/main/java/com/vbytsyuk/android/sandbox/MainActivity.kt
@@ -3,13 +3,16 @@ package com.vbytsyuk.android.sandbox
 import android.os.Bundle
 import android.widget.Button
 import com.vbytsyuk.android.core.activity.CoreActivity
+import com.vbytsyuk.android.core.activity.lazyFindViewById
 import com.vbytsyuk.android.core.activity.navigateTo
 import com.vbytsyuk.android.glide.GlideActivity
 
 
 class MainActivity : CoreActivity(layoutId = R.layout.activity_main) {
+    private val buttonGlide: Button by lazyFindViewById(R.id.amBtnGlide)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        findViewById<Button>(R.id.button).setOnClickListener { navigateTo<GlideActivity>() }
+        buttonGlide.setOnClickListener { navigateTo<GlideActivity>() }
     }
 }
