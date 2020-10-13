fc4f9625f1781a139f048404b582c052d6c64513
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-10-14 02:05:04 +0300

    Implemented Picasso example

--- app/pictures/src/main/java/com/vbytsyuk/android/pictures/loaders/GlidePicturesLoader.kt
+++ app/pictures/src/main/java/com/vbytsyuk/android/pictures/loaders/GlidePicturesLoader.kt
@@ -1,22 +1,25 @@
-package com.vbytsyuk.android.image.loaders
+package com.vbytsyuk.android.pictures.loaders
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
-import com.vbytsyuk.android.core.pictures.ImageLoader
+import com.vbytsyuk.android.core.pictures.PicturesLoader
+import com.vbytsyuk.android.pictures.R
 
 
-class GlideImageLoader(private val context: Context) : ImageLoader {
+class GlidePicturesLoader(private val context: Context) : PicturesLoader {
     override fun clear(view: ImageView) {
         Glide.with(context).clear(view)
     }
 
     override fun load(imageUrl: String, intoView: ImageView) {
-        Glide.with(context).load(imageUrl).into(intoView)
+        Glide.with(context).load(imageUrl).error(R.drawable.ic_error).into(intoView)
     }
 
     override fun load(@DrawableRes drawableId: Int, intoView: ImageView) {
-        Glide.with(context).load(drawableId).into(intoView)
+        Glide.with(context).load(drawableId).error(R.drawable.ic_error).into(intoView)
     }
 }
--- app/pictures/src/main/java/com/vbytsyuk/android/pictures/loaders/PicassoPicturesLoader.kt
+++ app/pictures/src/main/java/com/vbytsyuk/android/pictures/loaders/PicassoPicturesLoader.kt
@@ -1,4 +1,30 @@
 package com.vbytsyuk.android.pictures.loaders
 
-class PicassoPicturesLoader {
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import com.vbytsyuk.android.core.pictures.PicturesLoader
+import com.vbytsyuk.android.pictures.R
+import java.lang.Exception
+
+
+class PicassoPicturesLoader : PicturesLoader {
+    override fun clear(view: ImageView) {
+        view.setImageResource(0)
+    }
+
+    override fun load(imageUrl: String, intoView: ImageView) {
+        Picasso.get().load(imageUrl).into(intoView, callback(intoView))
+    }
+
+    /* Picasso can't work with vector drawables. */
+    override fun load(@DrawableRes drawableId: Int, intoView: ImageView) {
+        Picasso.get().load(drawableId).into(intoView, callback(intoView))
+    }
+
+    private fun callback(view: ImageView) = object : Callback {
+        override fun onSuccess() { /* do nothing */ }
+        override fun onError(e: Exception?) = view.setImageResource(R.drawable.ic_error)
+    }
 }
