71a5abb43d29289f9434a4a47e845dfd4a00140d
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-10-23 01:25:26 +0300

    Create module for lifecycle

---
+++ app/lifecycle/src/main/java/com/vbytsyuk/android/lifecycle/ChooseLifecycleActivity.kt
@@ -0,0 +1,22 @@
+package com.vbytsyuk.android.lifecycle
+
+import android.widget.Button
+import com.vbytsyuk.android.core.activity.CoreActivity
+import com.vbytsyuk.android.core.activity.lazyFindViewById
+import com.vbytsyuk.android.core.appbar.AppBarConfigurator
+import com.vbytsyuk.android.core.appbar.ToolBarConfigurator
+
+
+class ChooseLifecycleActivity : CoreActivity(layoutId = R.layout.activity_choose_lifecycle) {
+    private val buttonSimpleActivity: Button by lazyFindViewById(R.id.aclBtnSimpleActivity)
+
+    override val appBarConfigurator = ToolBarConfigurator(
+        toolbarId = R.id.aclToolbar,
+        titleId = R.string.acl_title,
+        leftButton = AppBarConfigurator.LeftButton(R.drawable.ic_arrow_back) { finish() }
+    )
+
+    override fun setClickListeners() {
+        buttonSimpleActivity.setOnClickListener {  }
+    }
+}
--- app/src/main/java/com/vbytsyuk/android/sandbox/MainActivity.kt
+++ app/src/main/java/com/vbytsyuk/android/sandbox/MainActivity.kt
@@ -6,11 +6,13 @@ import com.vbytsyuk.android.core.activity.CoreActivity
 import com.vbytsyuk.android.core.activity.lazyFindViewById
 import com.vbytsyuk.android.core.activity.navigateTo
 import com.vbytsyuk.android.core.appbar.AppBarConfigurator
+import com.vbytsyuk.android.lifecycle.ChooseLifecycleActivity
 import com.vbytsyuk.android.pictures.screen.PictureScreenActivity
 
 
 class MainActivity : CoreActivity(layoutId = R.layout.activity_main) {
     private val buttonImageLoaders: Button by lazyFindViewById(R.id.amBtnPictureLoaders)
+    private val buttonLifecycle: Button by lazyFindViewById(R.id.amBtnLifeCycle)
 
     override val appBarConfigurator = ToolBarConfigurator(
         toolbarId = R.id.amToolbar,
@@ -19,5 +21,6 @@ class MainActivity : CoreActivity(layoutId = R.layout.activity_main) {
 
     override fun setClickListeners() {
         buttonImageLoaders.setOnClickListener { navigateTo<PictureScreenActivity>() }
+        buttonLifecycle.setOnClickListener { navigateTo<ChooseLifecycleActivity>() }
     }
 }
