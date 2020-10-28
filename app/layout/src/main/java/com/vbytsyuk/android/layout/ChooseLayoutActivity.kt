2c9da9b5549dff902087c736c568cba11b07306b
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-10-29 00:59:08 +0300

    Added Classic layout activity

--- app/layout/src/main/java/com/vbytsyuk/android/layout/ChooseLayoutActivity.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ChooseLayoutActivity.kt
@@ -3,12 +3,13 @@ package com.vbytsyuk.android.layout
 import android.widget.Button
 import com.vbytsyuk.android.core.activity.CoreActivity
 import com.vbytsyuk.android.core.activity.lazyFindViewById
+import com.vbytsyuk.android.core.activity.navigateTo
 
 //https://www.figma.com/file/EYV3UeSa7vnjWEr8txbol5YV/App-cart-templates-ecommerce/duplicate
 class ChooseLayoutActivity : CoreActivity(layoutId = R.layout.activity_choose_layout) {
     private val buttonClassicLayout: Button by lazyFindViewById(R.id.aclBtnClassicLayout)
 
     override fun setClickListeners() {
-        buttonClassicLayout.setOnClickListener {  }
+        buttonClassicLayout.setOnClickListener { navigateTo<ClassicLayoutActivity>() }
     }
 }
---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ClassicLayoutActivity.kt
@@ -0,0 +1,10 @@
+package com.vbytsyuk.android.layout
+
+import com.vbytsyuk.android.core.activity.CoreActivity
+
+
+class ClassicLayoutActivity : CoreActivity(layoutId = R.layout.activity_classic_layout) {
+    override fun setClickListeners() {
+        /* do nothing */
+    }
+}
