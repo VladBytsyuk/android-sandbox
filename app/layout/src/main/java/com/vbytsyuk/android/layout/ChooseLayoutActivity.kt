6a83d02f92efec85c32cb0a1fefc5473c21dac77
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-10-29 01:04:29 +0300

    Added constraint layout activity

--- app/layout/src/main/java/com/vbytsyuk/android/layout/ChooseLayoutActivity.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ChooseLayoutActivity.kt
@@ -8,8 +8,10 @@ import com.vbytsyuk.android.core.activity.navigateTo
 //https://www.figma.com/file/EYV3UeSa7vnjWEr8txbol5YV/App-cart-templates-ecommerce/duplicate
 class ChooseLayoutActivity : CoreActivity(layoutId = R.layout.activity_choose_layout) {
     private val buttonClassicLayout: Button by lazyFindViewById(R.id.aclBtnClassicLayout)
+    private val buttonConstraintLayout: Button by lazyFindViewById(R.id.aclBtnConstraintLayout)
 
     override fun setClickListeners() {
         buttonClassicLayout.setOnClickListener { navigateTo<ClassicLayoutActivity>() }
+        buttonConstraintLayout.setOnClickListener { navigateTo<ConstraintLayoutActivity>() }
     }
 }
---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ConstraintLayoutActivity.kt
@@ -0,0 +1,10 @@
+package com.vbytsyuk.android.layout
+
+import com.vbytsyuk.android.core.activity.CoreActivity
+
+
+class ConstraintLayoutActivity : CoreActivity(layoutId = R.layout.activity_constraint_layout) {
+    override fun setClickListeners() {
+        /* do nothing */
+    }
+}
