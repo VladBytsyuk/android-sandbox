bd1888485c207e5a0a8c95879bc8ce9bcc883896
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-16 00:44:09 +0300

    Implemented compose preview

--- app/layout/src/main/java/com/vbytsyuk/android/layout/ComposeLayoutActivity.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ComposeLayoutActivity.kt
@@ -3,15 +3,20 @@ package com.vbytsyuk.android.layout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.setContent
 import androidx.ui.tooling.preview.Preview
 
+
 class ComposeLayoutActivity : AppCompatActivity() {
-    @Preview
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Text("Hello, World!")
-        }
+        setContent { Root() }
+    }
+
+    @Preview @Composable fun Preview() = Root()
+
+    @Composable fun Root() {
+        Text("Hello, World!")
     }
 }
