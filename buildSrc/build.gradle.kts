01ff837bea12aeffa0af6ad60ce6425be78fb710
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-15 13:00:27 +0300

    Update garde pugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
 dependencies {
     /* Example Dependency */
     /* Depend on the android gradle plugin, since we want to access it in our plugin */
-    implementation("com.android.tools.build:gradle:3.5.0")
+    implementation("com.android.tools.build:gradle:4.1.1")
 
     /* Example Dependency */
     /* Depend on the kotlin plugin, since we want to access it in our plugin */
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
 
     /* Depend on the default Gradle API's since we want to build a custom plugin */
     implementation(gradleApi())
