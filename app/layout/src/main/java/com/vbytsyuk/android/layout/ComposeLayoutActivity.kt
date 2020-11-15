f2975338a3893179054ccf4ea6dde9a5c4141885
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-16 01:07:52 +0300

    Added simple toolbar

--- app/layout/src/main/java/com/vbytsyuk/android/layout/ComposeLayoutActivity.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ComposeLayoutActivity.kt
@@ -2,9 +2,17 @@ package com.vbytsyuk.android.layout
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.preferredHeight
+import androidx.compose.foundation.layout.preferredSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 
@@ -13,10 +21,38 @@ class ComposeLayoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent { Root() }
     }
+}
 
-    @Preview @Composable fun Preview() = Root()
+@Preview @Composable fun Preview() = Root()
 
-    @Composable fun Root() {
-        Text("Hello, World!")
-    }
+
+@Composable fun Root() = ConstraintLayout {
+    val (bkg, toolbar) = createRefs()
+    Image(
+        asset = imageResource(id = R.drawable.img_header_bkg_light),
+        modifier = Modifier
+            .preferredHeight(256.dp)
+            .constrainAs(bkg) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                end.linkTo(parent.end)
+            }
+    )
+    ToolBar(
+        modifier = Modifier
+            .preferredHeight(dimensionResource(id = R.dimen.tool_bar_height))
+            .constrainAs(toolbar) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                end.linkTo(parent.end)
+            }
+    )
+}
+
+
+@Composable fun ToolBar(modifier: Modifier) = ConstraintLayout(modifier) {
+    Image(
+        asset = vectorResource(id = R.drawable.ic_toolbar_hamburger),
+        modifier = Modifier.preferredSize(dimensionResource(id = R.dimen.tool_bar_icon_size))
+    )
 }
