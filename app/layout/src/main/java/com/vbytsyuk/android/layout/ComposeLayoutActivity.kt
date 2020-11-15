23528c9aa107831ac58c0b8a71b3feefaa63b4ab
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-15 13:27:37 +0300

    Implemented simple layout

--- app/layout/src/main/java/com/vbytsyuk/android/layout/ComposeLayoutActivity.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ComposeLayoutActivity.kt
@@ -1,6 +1,17 @@
 package com.vbytsyuk.android.layout
 
-import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
-
-class ComposeLayoutActivity : Activity()
+class ComposeLayoutActivity : AppCompatActivity() {
+    @Preview
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello, World!")
+        }
+    }
+}
