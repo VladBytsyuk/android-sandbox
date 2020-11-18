91c74afdb6bad6f12db301291d128902e92a45de
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-19 01:19:28 +0300

    Added Theme attributes interface

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/Theme.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/Theme.kt
@@ -1,18 +1,45 @@
 package com.vbytsyuk.android.layout.compose
 
+import android.graphics.drawable.Drawable
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.graphics.Color
 
 
 @Composable
-fun Theme(content: @Composable () -> Unit) =
-    MaterialTheme(colors = Theme.Colors(), typography = Theme.Typography(), shapes = Theme.Shapes(), content = content)
+fun Theme(content: @Composable () -> Unit) = MaterialTheme(
+    colors = MaterialTheme.colors,
+    typography = Theme.typography(),
+    shapes = Theme.shapes(),
+    content = content
+)
+
+enum class ThemeMode { LIGHT, DARK }
+
+interface ThemeAttributes {
+    val disabledBackground: Color
+    val enabledBackground: Color
+
+    val bottomBarIconTintDisabled: Color
+    val bottomBarIconTintEnabled: Color
+
+    val activeTabLineColor: Color
+
+    val headerBackgroundDrawable: Drawable
+    val iconTint: Color
+
+    val textColorBase: Color
+    val textColorSuccess: Color
+    val textColorFailure: Color
+
+    val screenBackground: Color get() = disabledBackground
+    val appBarBackground: Color get() = enabledBackground
+    val bottomBarBackground: Color get() = enabledBackground
+    val tabElementBackground: Color get() = enabledBackground
+    val tabContentBackground: Color get() = enabledBackground
+}
 
 private object Theme {
-    @Composable fun Colors() = MaterialTheme.colors
-    @Composable fun Typography() = MaterialTheme.typography
-    @Composable fun Shapes() = MaterialTheme.shapes
+    @Composable fun typography() = MaterialTheme.typography
+    @Composable fun shapes() = MaterialTheme.shapes
 }
