08eadb9215328fb4258baa7088c9c9c29fee9686
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-20 01:39:54 +0300

    Added Yellow color palette

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/colors/MaterialYellow.kt
@@ -0,0 +1,20 @@
+package com.vbytsyuk.android.layout.compose.colors
+
+
+object MaterialYellow : MaterialPalette {
+    override val _50 = MaterialColor(light = 0xffffff, normal = 0xfffde7, dark = 0xcccab5)
+    override val _100 = MaterialColor(light = 0xfffff7, normal = 0xfff9c4, dark = 0xcbc693)
+    override val _200 = MaterialColor(light = 0xffffcf, normal = 0xfff59d, dark = 0xcbc26d)
+    override val _300 = MaterialColor(light = 0xffffa8, normal = 0xfff176, dark = 0xcabf45)
+    override val _400 = MaterialColor(light = 0xffff8b, normal = 0xffee58, dark = 0xc9bc1f)
+    override val _500 = MaterialColor(light = 0xffff72, normal = 0xffeb3b, dark = 0xc8b900)
+    override val _600 = MaterialColor(light = 0xffff6b, normal = 0xfdd835, dark = 0xc6a700)
+    override val _700 = MaterialColor(light = 0xfff263, normal = 0xfbc02d, dark = 0xc49000)
+    override val _800 = MaterialColor(light = 0xffd95a, normal = 0xf9a825, dark = 0xc17900)
+    override val _900 = MaterialColor(light = 0xffb04c, normal = 0xf57f17, dark = 0xbc5100)
+
+    override val _a100 = MaterialColor(light = 0xffffbf, normal = 0xffff8d, dark = 0xcacc5d)
+    override val _a200 = MaterialColor(light = 0xffff5a, normal = 0xffff00, dark = 0xc7cc00)
+    override val _a400 = MaterialColor(light = 0xffff56, normal = 0xffea00, dark = 0xc7b800)
+    override val _a700 = MaterialColor(light = 0xffff52, normal = 0xffd600, dark = 0xc7a500)
+}
