dd26294eca88cb201ccefdfd02d3136cfdfd2d2c
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-20 01:04:52 +0300

    Added Green color palette

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/colors/MaterialGreen.kt
@@ -0,0 +1,20 @@
+package com.vbytsyuk.android.layout.compose.colors
+
+
+object MaterialGreen : MaterialPalette {
+    override val _50 = MaterialColor(light = 0xffffff, normal = 0xe8f5e9, dark = 0xb6c2b7)
+    override val _100 = MaterialColor(light = 0xfbfffc, normal = 0xc8e6c9, dark = 0x97b498)
+    override val _200 = MaterialColor(light = 0xd7ffd9, normal = 0xa5d6a7, dark = 0x75a478)
+    override val _300 = MaterialColor(light = 0xb2fab4, normal = 0x81c784, dark = 0x519657)
+    override val _400 = MaterialColor(light = 0x98ee99, normal = 0x66bb6a, dark = 0x338a3e)
+    override val _500 = MaterialColor(light = 0x80e27e, normal = 0x4caf50, dark = 0x087f23)
+    override val _600 = MaterialColor(light = 0x76d275, normal = 0x43a047, dark = 0x00701a)
+    override val _700 = MaterialColor(light = 0x6abf69, normal = 0x388e3c, dark = 0x00600f)
+    override val _800 = MaterialColor(light = 0x60ad5e, normal = 0x2e7d32, dark = 0x005005)
+    override val _900 = MaterialColor(light = 0x4c8c4a, normal = 0x1b5e20, dark = 0x003300)
+
+    override val _a100 = MaterialColor(light = 0xecfffd, normal = 0xb9f6ca, dark = 0x88c399)
+    override val _a200 = MaterialColor(light = 0x9fffe0, normal = 0x69f0ae, dark = 0x2bbd7e)
+    override val _a400 = MaterialColor(light = 0x66ffa6, normal = 0x00e676, dark = 0x00b248)
+    override val _a700 = MaterialColor(light = 0x5efc82, normal = 0x00c853, dark = 0x009624)
+}
