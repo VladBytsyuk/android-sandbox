379bc216d309beeca8a8f1035ce12524a3f41abf
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-20 01:50:24 +0300

    Fixed color base classes

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/palette/color/ColorShades.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/palette/color/ColorShades.kt
@@ -1,2 +1,15 @@
 package com.vbytsyuk.android.layout.compose.palette.color
 
+import androidx.annotation.ColorInt
+import androidx.compose.ui.graphics.Color
+
+
+class ColorShades(
+    @ColorInt light: Int,
+    @ColorInt normal: Int,
+    @ColorInt dark: Int
+) {
+    val light = Color(light)
+    val normal = Color(normal)
+    val dark = Color(dark)
+}
--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/palette/color/MaterialColor.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/palette/color/MaterialColor.kt
@@ -1,33 +1,20 @@
 package com.vbytsyuk.android.layout.compose.palette.color
 
-import androidx.annotation.ColorInt
-import androidx.compose.ui.graphics.Color
 
+interface MaterialColor {
+    val _50: ColorShades
+    val _100: ColorShades
+    val _200: ColorShades
+    val _300: ColorShades
+    val _400: ColorShades
+    val _500: ColorShades
+    val _600: ColorShades
+    val _700: ColorShades
+    val _800: ColorShades
+    val _900: ColorShades
 
-interface MaterialPalette {
-    val _50: MaterialColor
-    val _100: MaterialColor
-    val _200: MaterialColor
-    val _300: MaterialColor
-    val _400: MaterialColor
-    val _500: MaterialColor
-    val _600: MaterialColor
-    val _700: MaterialColor
-    val _800: MaterialColor
-    val _900: MaterialColor
-
-    val _a100: MaterialColor
-    val _a200: MaterialColor
-    val _a400: MaterialColor
-    val _a700: MaterialColor
-}
-
-class MaterialColor(
-    @ColorInt light: Int,
-    @ColorInt normal: Int,
-    @ColorInt dark: Int
-) {
-    val light = Color(light)
-    val normal = Color(normal)
-    val dark = Color(dark)
+    val _a100: ColorShades
+    val _a200: ColorShades
+    val _a400: ColorShades
+    val _a700: ColorShades
 }
