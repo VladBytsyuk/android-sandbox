7e7b36d4659cd85e43884169f27915b7d3672fdf
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-20 01:07:42 +0300

    Added Grey color palette

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/colors/MaterialGrey.kt
@@ -0,0 +1,20 @@
+package com.vbytsyuk.android.layout.compose.colors
+
+
+object MaterialGrey : MaterialPalette {
+    override val _50 = MaterialColor(light = 0xffffff, normal = 0xfafafa, dark = 0xc7c7c7)
+    override val _100 = MaterialColor(light = 0xffffff, normal = 0xf5f5f5, dark = 0xc2c2c2)
+    override val _200 = MaterialColor(light = 0xffffff, normal = 0xeeeeee, dark = 0xbcbcbc)
+    override val _300 = MaterialColor(light = 0xffffff, normal = 0xe0e0e0, dark = 0xaeaeae)
+    override val _400 = MaterialColor(light = 0xefefef, normal = 0xbdbdbd, dark = 0x8d8d8d)
+    override val _500 = MaterialColor(light = 0xcfcfcf, normal = 0x9e9e9e, dark = 0x707070)
+    override val _600 = MaterialColor(light = 0xa4a4a4, normal = 0x757575, dark = 0x494949)
+    override val _700 = MaterialColor(light = 0x8e8e8e, normal = 0x616161, dark = 0x373737)
+    override val _800 = MaterialColor(light = 0x6d6d6d, normal = 0x424242, dark = 0x1b1b1b)
+    override val _900 = MaterialColor(light = 0x484848, normal = 0x212121, dark = 0x000000)
+
+    override val _a100 = _100
+    override val _a200 = _200
+    override val _a400 = _400
+    override val _a700 = _700
+}
