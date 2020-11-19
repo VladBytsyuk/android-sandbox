572801ea2ca86ea3aa11b6503cfb7955a1824762
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-20 01:42:34 +0300

    Moved all colors into `color` package
    Moved all colors into `color` package

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/palette/Palette.kt
@@ -0,0 +1,2 @@
+package com.vbytsyuk.android.layout.compose.palette
+
---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/palette/color/MaterialLime.kt
@@ -0,0 +1,20 @@
+package com.vbytsyuk.android.layout.compose.palette.color
+
+
+object MaterialLime : MaterialPalette {
+    override val _50 = MaterialColor(light = 0xffffff, normal = 0xf9fbe7, dark = 0xc6c8b5)
+    override val _100 = MaterialColor(light = 0xfffff6, normal = 0xf0f4c3, dark = 0xbdc192)
+    override val _200 = MaterialColor(light = 0xffffce, normal = 0xe6ee9c, dark = 0xb3bc6d)
+    override val _300 = MaterialColor(light = 0xffffa6, normal = 0xdce775, dark = 0xa8b545)
+    override val _400 = MaterialColor(light = 0xffff89, normal = 0xd4e157, dark = 0xa0af22)
+    override val _500 = MaterialColor(light = 0xffff6e, normal = 0xcddc39, dark = 0x99aa00)
+    override val _600 = MaterialColor(light = 0xf5fd67, normal = 0xc0ca33, dark = 0x8c9900)
+    override val _700 = MaterialColor(light = 0xe4e65e, normal = 0xafb42b, dark = 0x7c8500)
+    override val _800 = MaterialColor(light = 0xd2ce56, normal = 0x9e9d24, dark = 0x6c6f00)
+    override val _900 = MaterialColor(light = 0xb4a647, normal = 0x827717, dark = 0x524c00)
+
+    override val _a100 = MaterialColor(light = 0xffffb3, normal = 0xf4ff81, dark = 0xbfcc50)
+    override val _a200 = MaterialColor(light = 0xffff78, normal = 0xeeff41, dark = 0xb8cc00)
+    override val _a400 = MaterialColor(light = 0xfdff58, normal = 0xc6ff00, dark = 0x90cc00)
+    override val _a700 = MaterialColor(light = 0xe4ff54, normal = 0xaeea00, dark = 0x79b700)
+}
