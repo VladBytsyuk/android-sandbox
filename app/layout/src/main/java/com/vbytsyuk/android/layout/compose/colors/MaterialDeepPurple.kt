aef9f037c26fcfc2cdaee7fba166c92402982ecf
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-20 01:01:58 +0300

    Added DeepPurple color palette

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/colors/MaterialDeepPurple.kt
@@ -0,0 +1,20 @@
+package com.vbytsyuk.android.layout.compose.colors
+
+
+object MaterialDeepPurple : MaterialPalette {
+    override val _50 = MaterialColor(light = 0xffffff, normal = 0xede7f6, dark = 0xbbb5c3)
+    override val _100 = MaterialColor(light = 0xfff7ff, normal = 0xd1c4e9, dark = 0xa094b7)
+    override val _200 = MaterialColor(light = 0xe6ceff, normal = 0xb39ddb, dark = 0x836fa9)
+    override val _300 = MaterialColor(light = 0xc7a4ff, normal = 0x9575cd, dark = 0x65499c)
+    override val _400 = MaterialColor(light = 0xb085f5, normal = 0x7e57c2, dark = 0x4d2c91)
+    override val _500 = MaterialColor(light = 0x9a67ea, normal = 0x673ab7, dark = 0x320b86)
+    override val _600 = MaterialColor(light = 0x9162e4, normal = 0x5e35b1, dark = 0x280680)
+    override val _700 = MaterialColor(light = 0x8559da, normal = 0x512da8, dark = 0x140078)
+    override val _800 = MaterialColor(light = 0x7953d2, normal = 0x4527a0, dark = 0x000070)
+    override val _900 = MaterialColor(light = 0x6746c3, normal = 0x311b92, dark = 0x000063)
+
+    override val _a100 = MaterialColor(light = 0xe7b9ff, normal = 0xb388ff, dark = 0x805acb)
+    override val _a200 = MaterialColor(light = 0xb47cff, normal = 0x7c4dff, dark = 0x3f1dcb)
+    override val _a400 = MaterialColor(light = 0xa255ff, normal = 0x651fff, dark = 0x0100ca)
+    override val _a700 = MaterialColor(light = 0x9d46ff, normal = 0x6200ea, dark = 0x0a00b6)
+}
