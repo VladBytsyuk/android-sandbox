1f34e603e361b193a4d4818c01f68dbc82534ab4
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-23 00:37:45 +0300

    Added HeaderInfo element

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/HeaderInfo.kt
@@ -0,0 +1,40 @@
+package com.vbytsyuk.android.layout.compose
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.ui.tooling.preview.Preview
+import com.vbytsyuk.android.layout.R
+
+
+@Preview
+@Composable
+private fun LightHeaderInfoBlockPreview() =
+    HeaderInfo(LightThemeAttributes,
+        ordersTitleId = R.string.header_orders_title, ordersValueId = R.string.header_orders_value,
+        ratingTitleId = R.string.header_rating_title, ratingValueId = R.string.header_rating_value,
+        purchasesTitleId = R.string.header_purchases_title, purchasesValueId = R.string.header_purchases_value,
+    )
+@Preview
+@Composable
+private fun DarkHeaderInfoBlockPreview() =
+    HeaderInfo(DarkThemeAttributes,
+        ordersTitleId = R.string.header_orders_title, ordersValueId = R.string.header_orders_value,
+        ratingTitleId = R.string.header_rating_title, ratingValueId = R.string.header_rating_value,
+        purchasesTitleId = R.string.header_purchases_title, purchasesValueId = R.string.header_purchases_value,
+    )
+
+@Composable
+fun HeaderInfo(
+    attr: ThemeAttributes,
+    @StringRes ordersTitleId: Int, @StringRes ordersValueId: Int,
+    @StringRes ratingTitleId: Int, @StringRes ratingValueId: Int,
+    @StringRes purchasesTitleId: Int, @StringRes purchasesValueId: Int,
+    modifier: Modifier = Modifier
+) = Column(
+    modifier = modifier.wrapContentHeight()
+) {
+
+}
