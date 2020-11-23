663b6810c2ee07535061ba6438f71844c2f3c47f
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-24 00:12:06 +0300

    Added constraints to Tab title

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/Tab.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/Tab.kt
@@ -3,12 +3,15 @@ package com.vbytsyuk.android.layout.compose.tabs
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawOpacity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
 import com.vbytsyuk.android.layout.R
@@ -34,6 +37,15 @@ fun Tab(
             fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
             color = attr.textColorBase
-        )
+        ),
+        modifier = Modifier
+            .constrainAs(title) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                end.linkTo(parent.end)
+                bottom.linkTo(parent.bottom)
+            }
+            .padding(16.dp)
+            .drawOpacity(0.87f)
     )
 }
