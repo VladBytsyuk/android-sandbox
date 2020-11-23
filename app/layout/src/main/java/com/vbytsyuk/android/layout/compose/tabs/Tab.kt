30452c3a920584fc09f72729e86a298fdc2f2d5b
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-24 00:02:51 +0300

    Added Tab composable function

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/Tab.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/Tab.kt
@@ -1,4 +1,20 @@
 package com.vbytsyuk.android.layout.compose.tabs
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.ui.tooling.preview.Preview
+import com.vbytsyuk.android.layout.compose.DarkThemeAttributes
+import com.vbytsyuk.android.layout.compose.LightThemeAttributes
+import com.vbytsyuk.android.layout.compose.ThemeAttributes
 
 
+@Preview @Composable fun LightTab() = Tab(LightThemeAttributes)
+@Preview @Composable fun DarkTab() = Tab(DarkThemeAttributes)
+
+@Composable
+fun Tab(
+    attr: ThemeAttributes,
+    modifier: Modifier = Modifier
+) {
+
+}
