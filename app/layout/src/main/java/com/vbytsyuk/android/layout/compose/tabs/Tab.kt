d47d86c02a852af41e36dd241cc16f71de9e9131
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-24 00:05:59 +0300

    Added Tab text

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/Tab.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/Tab.kt
@@ -1,20 +1,29 @@
 package com.vbytsyuk.android.layout.compose.tabs
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.ConstraintLayout
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.ui.tooling.preview.Preview
+import com.vbytsyuk.android.layout.R
 import com.vbytsyuk.android.layout.compose.DarkThemeAttributes
 import com.vbytsyuk.android.layout.compose.LightThemeAttributes
 import com.vbytsyuk.android.layout.compose.ThemeAttributes
 
 
-@Preview @Composable private fun LightTab() = Tab(LightThemeAttributes)
-@Preview @Composable private fun DarkTab() = Tab(DarkThemeAttributes)
+@Preview @Composable private fun LightTab() = Tab(LightThemeAttributes, R.string.tab_account)
+@Preview @Composable private fun DarkTab() = Tab(DarkThemeAttributes, R.string.tab_account)
 
 @Composable
 fun Tab(
     attr: ThemeAttributes,
+    @StringRes titleId: Int,
     modifier: Modifier = Modifier
-) {
-
+) = ConstraintLayout {
+    val (title, line) = createRefs()
+    Text(
+        text = stringResource(id = titleId)
+    )
 }
