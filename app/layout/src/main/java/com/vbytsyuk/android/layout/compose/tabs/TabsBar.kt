c08743f0d13dee1989e9d760212c786688ce5ede
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-26 00:02:34 +0300

    Fixed preview

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/TabsBar.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/tabs/TabsBar.kt
@@ -1,9 +1,14 @@
 package com.vbytsyuk.android.layout.compose.tabs
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import com.vbytsyuk.android.layout.R
 import com.vbytsyuk.android.layout.compose.DarkThemeAttributes
 import com.vbytsyuk.android.layout.compose.LightThemeAttributes
 import com.vbytsyuk.android.layout.compose.ThemeAttributes
@@ -20,5 +25,7 @@ fun TabsBar(
 }
 
 
-@Preview @Composable private fun LightTabBar() = TabsBar(LightThemeAttributes)
-@Preview @Composable private fun DarkTabsBar() = TabsBar(DarkThemeAttributes)
+@Preview @Composable private fun LightTabBar() =
+    TabsBar(LightThemeAttributes, modifier = Modifier.background(LightThemeAttributes.screenBackground))
+@Preview @Composable private fun DarkTabsBar() =
+    TabsBar(DarkThemeAttributes, modifier = Modifier.background(DarkThemeAttributes.screenBackground))
