4d0767330891aa1d11cd170f59a20b4220dbcc52
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-28 02:46:32 +0300

    Added filter text

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/OrdersHeader.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/OrdersHeader.kt
@@ -1,17 +1,33 @@
 package com.vbytsyuk.android.layout.compose
 
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.ConstraintLayout
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
+import com.vbytsyuk.android.layout.R
 
 
 @Composable
 fun OrdersHeader(
     attr: ThemeAttributes,
     modifier: Modifier = Modifier
-) = Row {
-
+) = ConstraintLayout() {
+    val (filter, icon, clear) = createRefs()
+    Text(
+        text = stringResource(id = R.string.tab_orders_header_picker),
+        modifier = Modifier
+            .padding(16.dp)
+            .constrainAs(filter) {
+                start.linkTo(parent.start)
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+            }
+    )
 }
 
 @Preview @Composable private fun LightOrdersHeader() = OrdersHeader(LightThemeAttributes)
