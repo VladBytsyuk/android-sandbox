510f449732cf0cde90f2a70583b30aad021858cb
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-28 02:52:05 +0300

    Added filter icon

--- app/layout/src/main/java/com/vbytsyuk/android/layout/compose/OrdersHeader.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/OrdersHeader.kt
@@ -1,13 +1,16 @@
 package com.vbytsyuk.android.layout.compose
 
+import androidx.compose.foundation.Icon
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.ConstraintLayout
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawOpacity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
@@ -20,7 +23,9 @@ import com.vbytsyuk.android.layout.R
 fun OrdersHeader(
     attr: ThemeAttributes,
     modifier: Modifier = Modifier
-) = ConstraintLayout() {
+) = ConstraintLayout(
+    modifier = modifier.fillMaxWidth()
+) {
     val (filter, icon, clear) = createRefs()
     Text(
         text = stringResource(id = R.string.tab_orders_header_picker),
@@ -36,6 +41,16 @@ fun OrdersHeader(
                 start.linkTo(parent.start)
                 top.linkTo(parent.top)
                 bottom.linkTo(parent.bottom)
+            }
+    )
+    Icon(
+        asset = vectorResource(id = R.drawable.ic_collapse_arrow),
+        tint = attr.iconTint,
+        modifier = Modifier
+            .constrainAs(icon) {
+                start.linkTo(filter.end)
+                top.linkTo(filter.top)
+                bottom.linkTo(filter.bottom)
             }
     )
 }
