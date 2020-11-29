45ed105654d3f101810ab87b697b38f736b97f83
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-11-30 00:01:28 +0300

    Added OrdersList element

---
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/compose/orders/OrdersList.kt
@@ -0,0 +1,44 @@
+package com.vbytsyuk.android.layout.compose.orders
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.ui.tooling.preview.Preview
+import com.vbytsyuk.android.layout.compose.DarkThemeAttributes
+import com.vbytsyuk.android.layout.compose.LightThemeAttributes
+import com.vbytsyuk.android.layout.compose.ThemeAttributes
+
+
+@Composable
+fun OrdersList(
+    attr: ThemeAttributes,
+    orderDataList: List<OrderData>,
+    modifier: Modifier = Modifier
+) = Column(modifier = modifier.background(attr.enabledBackground)) {
+   
+}
+
+
+@Preview @Composable fun LightOrdersList() = OrdersList(
+    LightThemeAttributes,
+    orderDataList = listOf(
+        OrderData.Successful(34567),
+        OrderData.Successful(34567),
+        OrderData.Discount,
+        OrderData.Returned
+    )
+)
+
+@Preview @Composable fun DarkOrdersList() = OrdersList(
+    DarkThemeAttributes,
+    orderDataList = listOf(
+        OrderData.Successful(34567),
+        OrderData.Successful(34567),
+        OrderData.Discount,
+        OrderData.Returned
+    )
+)
