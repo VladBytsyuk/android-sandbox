316e6389c585845332f2362e6d1bb86a6ff63037
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-04 00:03:56 +0300

    Added toolbar to choose layout activity

--- app/layout/src/main/java/com/vbytsyuk/android/layout/ChooseLayoutActivity.kt
+++ app/layout/src/main/java/com/vbytsyuk/android/layout/ChooseLayoutActivity.kt
@@ -4,6 +4,8 @@ import android.widget.Button
 import com.vbytsyuk.android.core.activity.CoreActivity
 import com.vbytsyuk.android.core.activity.lazyFindViewById
 import com.vbytsyuk.android.core.activity.navigateTo
+import com.vbytsyuk.android.core.appbar.AppBarConfigurator
+import com.vbytsyuk.android.core.appbar.ToolBarConfigurator
 
 //https://www.figma.com/file/EYV3UeSa7vnjWEr8txbol5YV/App-cart-templates-ecommerce/duplicate
 class ChooseLayoutActivity : CoreActivity(
@@ -14,6 +16,12 @@ class ChooseLayoutActivity : CoreActivity(
     private val buttonClassicLayout: Button by lazyFindViewById(R.id.aclBtnClassicLayout)
     private val buttonConstraintLayout: Button by lazyFindViewById(R.id.aclBtnConstraintLayout)
     private val buttonComposeLayout: Button by lazyFindViewById(R.id.aclBtnComposeLayout)
+
+    override val appBarConfigurator = ToolBarConfigurator(
+        toolbarId = R.id.aclayToolbar,
+        titleId = R.string.aclay_title,
+        leftButton = AppBarConfigurator.LeftButton(R.drawable.ic_arrow_back) { finish() }
+    )
 
     override fun setClickListeners() {
         buttonClassicLayout.setOnClickListener { navigateTo<ClassicLayoutActivity>() }
