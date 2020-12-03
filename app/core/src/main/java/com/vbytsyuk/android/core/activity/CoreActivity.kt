749570442e7bfaf36260e7a17747ff1cd18f2422
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-03 23:23:15 +0300

    Clear unused imports

--- app/core/src/main/java/com/vbytsyuk/android/core/activity/CoreActivity.kt
+++ app/core/src/main/java/com/vbytsyuk/android/core/activity/CoreActivity.kt
@@ -1,17 +1,8 @@
 package com.vbytsyuk.android.core.activity
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.annotation.LayoutRes
 import androidx.annotation.StyleRes
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
-import com.vbytsyuk.android.core.R
-import com.vbytsyuk.android.core.Theme
-import com.vbytsyuk.android.core.appbar.AppBarConfigurator
-import com.vbytsyuk.android.core.controllers.ThemeController
-import org.koin.android.ext.android.inject
 
 
 abstract class CoreActivity(
