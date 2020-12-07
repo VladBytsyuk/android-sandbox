2507ecfe60d739cc03dfc4f79c7d1871c8ec448e
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-08 00:19:47 +0300

    Added Choose Library activity

---
+++ app/reactive/src/main/java/com/vbytsyuk/android/reactive/ChooseLibraryActivity.kt
@@ -0,0 +1,12 @@
+package com.vbytsyuk.android.reactive
+
+import com.vbytsyuk.android.core.activity.CoreActivity
+
+
+class ChooseLibraryActivity : CoreActivity(
+    layoutId = R.layout.activity_choose_library
+) {
+    override fun setClickListeners() {
+        TODO("Not yet implemented")
+    }
+}
