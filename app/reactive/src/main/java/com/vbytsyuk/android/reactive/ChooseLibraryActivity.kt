615dd28db103fc4d4abc60d09b5de02d0ef564c7
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-09 00:33:38 +0300

    Added RX button

--- app/reactive/src/main/java/com/vbytsyuk/android/reactive/ChooseLibraryActivity.kt
+++ app/reactive/src/main/java/com/vbytsyuk/android/reactive/ChooseLibraryActivity.kt
@@ -1,11 +1,17 @@
 package com.vbytsyuk.android.reactive
 
+import android.widget.Button
 import com.vbytsyuk.android.core.activity.CoreActivity
+import com.vbytsyuk.android.core.activity.lazyFindViewById
 
 
 class ChooseLibraryActivity : CoreActivity(
-    layoutId = R.layout.activity_choose_library
+    layoutId = R.layout.activity_choose_library,
+    themeLight = R.style.Theme_Sandbox_Light_NoActionBar,
+    themeDark = R.style.Theme_Sandbox_Dark_NoActionBar
 ) {
+    private val buttonClassicLayout: Button by lazyFindViewById(R.id.aclBtnRxJava)
+
     override fun setClickListeners() {
         TODO("Not yet implemented")
     }
