6446a6e393788da24251f41aef3402e783d9c658
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-09 00:36:16 +0300

    Added toolbar

--- app/reactive/src/main/java/com/vbytsyuk/android/reactive/ChooseLibraryActivity.kt
+++ app/reactive/src/main/java/com/vbytsyuk/android/reactive/ChooseLibraryActivity.kt
@@ -3,6 +3,8 @@ package com.vbytsyuk.android.reactive
 import android.widget.Button
 import com.vbytsyuk.android.core.activity.CoreActivity
 import com.vbytsyuk.android.core.activity.lazyFindViewById
+import com.vbytsyuk.android.core.appbar.AppBarConfigurator
+import com.vbytsyuk.android.core.appbar.ToolBarConfigurator
 
 
 class ChooseLibraryActivity : CoreActivity(
@@ -12,6 +14,12 @@ class ChooseLibraryActivity : CoreActivity(
 ) {
     private val buttonRxJava: Button by lazyFindViewById(R.id.aclBtnRxJava)
     private val buttonKotlinFlow: Button by lazyFindViewById(R.id.aclBtnFlow)
+
+    override val appBarConfigurator = ToolBarConfigurator(
+        toolbarId = R.id.aclibToolbar,
+        titleId = R.string.aclib_title,
+        leftButton = AppBarConfigurator.LeftButton(R.drawable.ic_arrow_back) { finish() }
+    )
 
     override fun setClickListeners() {
         TODO("Not yet implemented")
