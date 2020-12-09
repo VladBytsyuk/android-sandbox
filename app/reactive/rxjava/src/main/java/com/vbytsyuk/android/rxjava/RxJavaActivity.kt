72d2739805436aa82c0555b11c8197e02e09f646
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-10 00:18:36 +0300

    Added RxJava module activity

---
+++ app/reactive/rxjava/src/main/java/com/vbytsyuk/android/rxjava/RxJavaActivity.kt
@@ -0,0 +1,14 @@
+package com.vbytsyuk.android.rxjava
+
+import com.vbytsyuk.android.core.activity.CoreActivity
+
+
+class RxJavaActivity : CoreActivity(
+    layoutId = R.layout.activity_rx_java,
+    themeLight = R.style.Theme_Sandbox_Light_NoActionBar,
+    themeDark = R.style.Theme_Sandbox_Dark_NoActionBar
+) {
+    override fun setClickListeners() {
+        TODO("Not yet implemented")
+    }
+}
