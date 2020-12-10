93949eba9d02c46d93656f1a3b2cbbe91c080990
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-11 00:48:10 +0300

    Configure RxJava activity toolbar

--- app/reactive/rxjava/src/main/java/com/vbytsyuk/android/rxjava/RxJavaActivity.kt
+++ app/reactive/rxjava/src/main/java/com/vbytsyuk/android/rxjava/RxJavaActivity.kt
@@ -1,6 +1,8 @@
 package com.vbytsyuk.android.rxjava
 
 import com.vbytsyuk.android.core.activity.CoreActivity
+import com.vbytsyuk.android.core.appbar.AppBarConfigurator
+import com.vbytsyuk.android.core.appbar.ToolBarConfigurator
 
 
 class RxJavaActivity : CoreActivity(
@@ -8,6 +10,12 @@ class RxJavaActivity : CoreActivity(
     themeLight = R.style.Theme_Sandbox_Light_NoActionBar,
     themeDark = R.style.Theme_Sandbox_Dark_NoActionBar
 ) {
+    override val appBarConfigurator = ToolBarConfigurator(
+        toolbarId = R.id.arjToolbar,
+        titleId = R.string.arj_title,
+        leftButton = AppBarConfigurator.LeftButton(R.drawable.ic_arrow_back) { finish() }
+    )
+
     override fun setClickListeners() {
         TODO("Not yet implemented")
     }
