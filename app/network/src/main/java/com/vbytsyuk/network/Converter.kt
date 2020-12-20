9e8b7e30e10eb032f3008b9567305f7720579734
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-21 00:11:04 +0300

    Added converter implementation

--- app/network/src/main/java/com/vbytsyuk/network/Converter.kt
+++ app/network/src/main/java/com/vbytsyuk/network/Converter.kt
@@ -1,2 +1,22 @@
 package com.vbytsyuk.network
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.HttpException
+
+
+suspend fun <M : Any, R : Response<M>> makeApiCall(
+    block: suspend () -> R
+): ApiResult<M> =
+    try {
+        val response = withContext(Dispatchers.IO) { block() }
+        ApiResult.Received.Content(response.toModel())
+    } catch (httpException: HttpException) {
+        ApiResult.Received.Error(
+            code = httpException.code(),
+            description = httpException.message(),
+            message = httpException.response()?.errorBody()?.string()
+        )
+    } catch (throwable: Throwable) {
+        ApiResult.Failure(throwable)
+    }
