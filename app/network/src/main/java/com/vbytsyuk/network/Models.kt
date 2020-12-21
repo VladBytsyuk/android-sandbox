51ccf8d538055bbe750feb70982de7860d9055e5
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-22 01:10:19 +0300

    Added CatBreed model class

---
+++ app/network/src/main/java/com/vbytsyuk/network/Models.kt
@@ -0,0 +1,12 @@
+package com.vbytsyuk.network
+
+data class CatBreed(
+    val id: String,
+    val name: String,
+    val temperament: String,
+    val lifeSpan: String,
+    val alternativeNames: String,
+    val wikipediaUrl: String,
+    val origin: String,
+    val weightImperial: String,
+)
