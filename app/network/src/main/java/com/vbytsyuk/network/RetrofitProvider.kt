3f05617175f0a8332e7c200bf264074bb00eaeb6
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-22 01:19:43 +0300

    Implemented RetrofitProvider

--- app/network/src/main/java/com/vbytsyuk/network/RetrofitProvider.kt
+++ app/network/src/main/java/com/vbytsyuk/network/RetrofitProvider.kt
@@ -1,2 +1,16 @@
 package com.vbytsyuk.network
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+internal class RetrofitProvider(private val url: String) {
+    inline fun <reified T> create(): T = createRetrofit().create(T::class.java)
+    
+    private fun createRetrofit() = Retrofit.Builder()
+        .baseUrl(url)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(OkHttpClient.Builder().build())
+        .build()
+}
