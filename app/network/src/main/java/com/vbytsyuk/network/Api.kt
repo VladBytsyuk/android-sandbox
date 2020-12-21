3288e51716056695537fddae2d0663db425daa95
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2020-12-22 01:21:37 +0300

    Simple implementation of Api class

--- app/network/src/main/java/com/vbytsyuk/network/Api.kt
+++ app/network/src/main/java/com/vbytsyuk/network/Api.kt
@@ -1,4 +1,7 @@
 package com.vbytsyuk.network
 
 class Api {
+    private val catsApi: CatsApi = RetrofitProvider(url = "https://api.thecatapi.com/v1").create()
+
+    suspend fun fetchCatBreedsList(): ApiResult<List<CatBreed>> = makeApiCall { catsApi.fetchBreedsList() }
 }
--- app/network/src/main/java/com/vbytsyuk/network/RetrofitProvider.kt
+++ app/network/src/main/java/com/vbytsyuk/network/RetrofitProvider.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 internal class RetrofitProvider(private val url: String) {
     inline fun <reified T> create(): T = createRetrofit().create(T::class.java)
-    
+
     private fun createRetrofit() = Retrofit.Builder()
         .baseUrl(url)
         .addConverterFactory(GsonConverterFactory.create())
